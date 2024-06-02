e3714bdb1d689aa9ff7215182305e692cd3dc77f
TechMartial <sravanth7762@gmail.com>
2024-06-02 15:26:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,14 @@ document.getElementById('saveContact').addEventListener('click', function() {
   const contactNumber = '1231231231';
   const contactName = 'One Stop Solutions';
 
-  const contactData = {
-    name: contactName,
-    tel: [contactNumber]
-  };
+  const smsUrl = `sms://?body=Save%20${contactName}%20contact:%20${contactNumber}`;
 
-  if (navigator.contacts) {
-    navigator.contacts.create(contactData)
-      .then(function(contact) {
-        console.log('Contact saved:', contact);
-        alert('Contact saved successfully!');
-      })
-      .catch(function(error) {
-        console.error('Error saving contact:', error);
-        alert('Error saving contact. Please try again later.');
-      });
+  if (navigator.share) {
+    navigator.share({
+      url: smsUrl
+    })
+    .catch(console.error);
   } else {
-    console.error('Web Contact API not supported');
-    alert('Web Contact API is not supported in your browser.');
+    window.open(smsUrl, '_blank');
   }
 });
