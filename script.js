9f938dea6fc7e1180eba839ec332488323e70920
TechMartial <sravanth7762@gmail.com>
2024-06-02 15:31:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ document.getElementById('saveContact').addEventListener('click', function() {
   const contactNumber = '1231231231';
   const contactName = 'One Stop Solutions';
 
-  const smsUrl = `sms://?body=Save%20${contactName}%20contact:%20${contactNumber}`;
+  const contactData = {
+    name: contactName,
+    tel: contactNumber
+  };
 
-  if (navigator.share) {
-    navigator.share({
-      url: smsUrl
-    })
-    .catch(console.error);
-  } else {
-    window.open(smsUrl, '_blank');
-  }
+  const contactPicker = new google.contacts.ContactPicker(contactData);
+
+  contactPicker.openContactViewer();
 });
